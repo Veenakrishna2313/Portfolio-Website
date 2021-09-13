4ad8bd5123ff88632962c24c90aa87523e0988d9
Veena Krishna Sandana <43022790+Veenakrishna2313@users.noreply.github.com>
2021-09-13 00:44:13 -0700

    added header, particles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/navbar";
+import Header from "./components/Header";
+import Particles from "react-particles-js";
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles-canvas"
+      params={{
+        particles:{
+          number:{
+            value:30, density:{
+              enable:true, value_area:800
+            }
+          }, shape:{
+            type:"star", stroke:{
+              width:5, color:"#f9ab00"
+            }
+          }
+        }
+      }}/>
       <Navbar/>
-      
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+const Header = () => {
+  return (  
+    <div className="header-wraper">
+     <div className="main-info">
+    <h1>Hi, I'm Veena,</h1>
+      <h1> Full Stack Web Developer</h1>
+       
+       </div> 
+    </div>
+  );
+}
+ 
+export default Header;
