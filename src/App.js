3059194e900df200cc6e3684840646574d9d3f69
Veena Krishna Sandana <43022790+Veenakrishna2313@users.noreply.github.com>
2021-09-13 03:56:42 -0700

    experience added

--- src/App.js
+++ src/App.js
@@ -5,30 +5,37 @@ import Header from "./components/Header";
 import Particles from "react-particles-js";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Experience from "./components/Experience";
 
 function App() {
   return (
     <div className="App">
-      <Particles className="particles-canvas"
-      params={{
-        particles:{
-          number:{
-            value:30, density:{
-              enable:true, value_area:800
-            }
-          }, shape:{
-            type:"star", stroke:{
-              width:5, color:"#f9ab00"
-            }
-          }
-        }
-      }}/>
-      <Navbar/>
-      <Header/>
-      <About/>
-      <Skills/>
-
-
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+            shape: {
+              type: "star",
+              stroke: {
+                width: 5,
+                color: "#f9ab00",
+              },
+            },
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
+      <About />
+      <Skills />
+      <Experience />
     </div>
   );
 }
