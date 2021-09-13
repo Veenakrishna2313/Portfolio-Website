6257e47d8f4c28fd64b1d0106a61f848cba8689d
Veena Krishna Sandana <43022790+Veenakrishna2313@users.noreply.github.com>
2021-09-13 16:15:45 -0700

    added portfolio

---
+++ src/components/Portfolio.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+const Portfolio = () => {
+  return ( 
+    <div className="container">
+      <h1 className="text-uppercase text-center py-5">Portfolio</h1>
+<div className="image-box-wrapper roq justify-content-center">
+  <img className="portfolio-image" src="" alt="" />
+</div>
+
+    </div>
+   );
+}
+ 
+export default Portfolio;
