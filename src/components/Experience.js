04a0ec6db227f0bead757eb5a4b148914490c77e
VeenaKrishna2313 <veenakrishna.s@gmail.com>
2023-09-26 20:56:43 -0700

    Changed direction

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,10 +7,10 @@ const Experience = () => {
         <h1>Experience</h1>
       </div>
       <div className="container experience-wrapper">
-        <div className="timeline-block timeline-block-right">
+        <div className="timeline-block timeline-block-left">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2023 - Present</h3>
+            <h3>Aug 2023 - Present</h3>
             <h6>
               Web Developer |<i> The Donovan's Piano Room</i>
             </h6>
@@ -30,7 +30,7 @@ const Experience = () => {
             </p>
           </div>
         </div>
-        <div className="timeline-block timeline-block-left">
+        <div className="timeline-block timeline-block-right">
           <div className="marker"></div>
           <div className="timeline-content">
             <h3>2021 - Present</h3>
@@ -63,7 +63,7 @@ const Experience = () => {
             </p>
           </div>
         </div>
-        <div className="timeline-block timeline-block-right">
+        <div className="timeline-block timeline-block-left">
           <div className="marker"></div>
           <div className="timeline-content">
             <h3>April 2021-June 2022</h3>
